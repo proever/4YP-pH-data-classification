e23a17e7b3ec9c6411e8de4011e58af01d2a40e9
Paul Roever <proever@Pauls-MBP.lan>
2019-01-19 01:18:26 +0000

    add a basic keras training script

---
+++ trainer.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+
+import data_labeling
+
+data, labels = data_labeling.get_data_and_labels()
+
+data = data.reshape((393890, 150, 1))
+print(data.shape)
+
+model = tf.keras.Sequential()
+
+model.add(layers.Conv1D(
+    filters=16,
+    kernel_size=3,
+    padding='valid',
+    input_shape=(150, 1),
+    activation='relu'))
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+model.summary()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data, labels, test_size=0.20, random_state=42)
+
+model.fit(X_train, y_train, epochs=10)
+
+score = model.evaluate(X_test, y_test)
+
+print(score)
